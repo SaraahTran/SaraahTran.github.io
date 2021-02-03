ce271a4620f0ff3931e56410ac0e41b8814bc069
SaraahTran <77770132+SaraahTran@users.noreply.github.com>
2021-02-03 15:15:16 +1100

    Add files via upload

---
+++ current-portfolio.js
@@ -0,0 +1,32 @@
+
+//Nav
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
+
+
+function toggleDiv(divid)
+  {
+ 
+    varon = divid + 'on';
+    varoff = divid + 'off';
+ 
+    if(document.getElementById(varon).style.display == 'block')
+    {
+    document.getElementById(varon).style.display = 'none';
+    document.getElementById(varoff).style.display = 'block';
+    }
+   
+    else
+    {  
+    document.getElementById(varoff).style.display = 'none';
+    document.getElementById(varon).style.display = 'block'
+    }
+} 
